b14ff7c35cc036986b99604ce8d6b979976220fd
Aaron Blum <blumaa@gmail.com>
2019-07-06 18:27:16 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!\
 def greeting (name)
   puts "Hello, this is your greeting."
-  puts "Hello #{greeting}, it's nice to meet you."
+  puts "Hello #{greeting(name)}, it's nice to meet you."
 end
